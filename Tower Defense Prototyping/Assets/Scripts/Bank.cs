ec28628dba477d1a6ef6ac8b9a2a607336dbef39
The Potato <ultimate801@gmail.com>
2016-08-20 03:40:54 -0500

    lives and bank add

---
+++ Tower Defense Prototyping/Assets/Scripts/Bank.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bank : MonoBehaviour {
+    private int money;
+
+
+    void Start () {
+        money = 0;
+	}
+
+
+    void addMoney(int amount)
+    {
+        money = money + amount;
+    }
+
+
+    void subractMoney(int amount)
+    {
+        money = money - amount;
+    }
+
+    int getMoney()
+    {
+        return money;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Tower Defense Prototyping/Assets/Scripts/Lives.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lives : MonoBehaviour {
+    private int lives;
+    // Use this for initialization
+    void Start()
+    {
+        lives = 30;
+    }
+
+    void loseLife()
+    {
+        lives = lives - 1;
+    }
+
+	void addLives(int amount)
+    {
+        lives = lives + amount;
+    }
+
+    void loseLives(int amount)
+    {
+        lives = lives - amount;
+    }
+
+    int getLives()
+    {
+        return lives;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
